4dc19131936a14a62c2775709eb2324ff44068a9
Leonardo Calixta <leoatc@gmail.com>
2025-04-04 16:49:56 -0300

    Update super_trunfo.c
    Editado utilizando scanf

--- super_trunfo.c
+++ super_trunfo.c
@@ -6,42 +6,67 @@ int main(){
  char estado1[50] = "São Paulo";
  char estado2[50] = "Rio de Janeiro";
  char codigo1[50] = "A01";
- char codigo2[50] = "A02";
+ char codigo2[50] = "B01";
  char cidade1[50] = "Sao Paulo";
  char cidade2[50] = "Rio de Janeiro";
- int populacao1 = 12.000000;
- int populacao2 = 7.000000;
+ int populacao1 = 12000000;
+ int populacao2 = 7000000;
  float area1 = 120.000;
  float area2 = 45.000;
  float PIB1 = 200;
  float PIB2 = 75;
- int pontosT1 = 50;
- int pontosT2 = 25; 
+ int pontos_t1 = 50;
+ int pontos_t2 = 25; 
 
- //Organização das cartas A
+ //Organização das cartas A e B
 
  printf("CARTA A: \n");
- printf("Estado: %s \n" , estado1);
- printf("Carta: %s \n" , codigo1);
- printf("Cidade: %s \n" , cidade1);
- printf("População: %d \n" , populacao1);
- printf("Área: %f km² \n" , area1);
- printf("PIB: %f Bilhões de Reais \n" , PIB1);
- printf("Pontos Turísticos: %d \n" , pontosT1);
- //Cartas A finalizadas
-
+ scanf("Carta A = %s \n" , codigo1);
+ printf("A carta A é: %s \n" , codigo1);
+ printf("ESTADO:  \n");
+ scanf("Estado = %s \n" , estado1);
+ printf("O estado é: %s \n" , estado1);
+ printf("CIDADE: \n");
+ scanf("Cidade = %s \n" , cidade1);
+ printf("A cidade é: %s \n" , cidade1);
+ printf("POPULAÇÃO: \n");
+ scanf("População = %d \n" , populacao1);
+ printf("A população é: %d \n" , populacao1);
+ printf("ÁREA: \n");
+ scanf("Área = %f \n" , area1);
+ printf("A área é: %f \n" , area1);
+ printf("PIB: \n");
+ scanf("PIB = %f \n" , PIB1);
+ printf("O PIB é: %f \n" , PIB1);
+ printf("PONTOS TURÍSTICOS: \n");
+ scanf("Pontos turísticos = %d \n" , pontos_t1);
+ printf("A quatidade de pontos turísticos é: %d \n" , pontos_t1);
  printf(" \n");
-
-//Organização das cartas B
  printf("CARTA B: \n");
- printf("Estados: %s \n" , estado2);
- printf("Carta: %s \n" , codigo2);
- printf("Cidade: %s \n" , cidade2);
- printf("População: %d \n" , populacao2);
- printf("Área: %f km² \n" , area2);
- printf("PIB: %f Bilhões de Reais\n" , PIB2);
- printf("Pontos Turísticos: %d \n" , pontosT2);
-//Cartas B finalizadas
-return 0;
+ scanf("Carta B = %s \n" , codigo2);
+ printf("A carta B é: %s \n" , codigo2);
+ printf("ESTADO:  \n");
+ scanf("Estado = %s \n" , estado2);
+ printf("O estado é: %s \n" , estado2);
+ printf("CIDADE: \n");
+ scanf("Cidade = %s \n" , cidade2);
+ printf("A cidade é: %s \n" , cidade2);
+ printf("POPULAÇÃO: \n");
+ scanf("População = %d \n" , populacao2);
+ printf("A população é: %d \n" , populacao2);
+ printf("ÁREA: \n");
+ scanf("Área = %f \n" , area2);
+ printf("A área é: %f \n" , area2);
+ printf("PIB: \n");
+ scanf("PIB = %f \n" , PIB2);
+ printf("O PIB é: %f \n" , PIB2);
+ printf("PONTOS TURÍSTICOS: \n");
+ scanf("Pontos turísticos = %d \n" , pontos_t2);
+ printf("A quatidade de pontos turísticos é: %d \n" , pontos_t2);
+
+
+ //Cartas finalizadas
+
+ return 0;
 
 }
