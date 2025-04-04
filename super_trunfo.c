f9efbb87662155e4f7afb828276ce8b1cc32b2e8
Leonardo Calixta <leoatc@gmail.com>
2025-04-04 17:20:35 -0300

    Update super_trunfo.c
    Adicionado espaço entre as cartas com comentário

--- super_trunfo.c
+++ super_trunfo.c
@@ -9,11 +9,11 @@ int main(){
  char codigo2[50] = "B01";
  char cidade1[50] = "Sao Paulo";
  char cidade2[50] = "Rio de Janeiro";
- int populacao1 = 12000000;
- int populacao2 = 7000000;
- float area1 = 120.000;
- float area2 = 45.000;
- float PIB1 = 200;
+ int populacao1 = 12.0;
+ int populacao2 = 7.0;
+ float area1 = 120.0;
+ float area2 = 45.0;
+ float PIB1 = 20;
  float PIB2 = 75;
  int pontos_t1 = 50;
  int pontos_t2 = 25; 
@@ -31,17 +31,19 @@ int main(){
  printf("A cidade é: %s \n" , cidade1);
  printf("POPULAÇÃO: \n");
  scanf("População = %d \n" , &populacao1);
- printf("A população é: %d \n" , populacao1);
+ printf("A população é: %d milhões de habitantes\n" , populacao1);
  printf("ÁREA: \n");
- scanf("Área = %f \n" , &area1);
- printf("A área é: %f \n" , area1);
+ scanf("Área = %2f \n" , &area1);
+ printf("A área é: %2f km²\n" , area1);
  printf("PIB: \n");
- scanf("PIB = %f \n" , &PIB1);
- printf("O PIB é: %f \n" , PIB1);
+ scanf("PIB = %2f \n" , &PIB1);
+ printf("O PIB é: %2f bilhões de reais \n" , PIB1);
  printf("PONTOS TURÍSTICOS: \n");
  scanf("Pontos turísticos = %d \n" , &pontos_t1);
  printf("A quatidade de pontos turísticos é: %d \n" , pontos_t1);
+ // comando a seguir para dar mais espaço entre as cartas 
  printf(" \n");
+ // início das infos da carta B
  printf("CARTA B: \n");
  scanf("Carta B = %s \n" , &codigo2);
  printf("A carta B é: %s \n" , codigo2);
@@ -53,13 +55,13 @@ int main(){
  printf("A cidade é: %s \n" , cidade2);
  printf("POPULAÇÃO: \n");
  scanf("População = %d \n" , &populacao2);
- printf("A população é: %d \n" , populacao2);
+ printf("A população é: %d milhões de habitantes \n" , populacao2);
  printf("ÁREA: \n");
- scanf("Área = %f \n" , &area2);
- printf("A área é: %f \n" , area2);
+ scanf("Área = %2f \n" , &area2);
+ printf("A área é: %2f km²\n" , area2);
  printf("PIB: \n");
- scanf("PIB = %f \n" , &PIB2);
- printf("O PIB é: %f \n" , PIB2);
+ scanf("PIB = %2f \n" , &PIB2);
+ printf("O PIB é: %2f bilhões de reais \n" , PIB2);
  printf("PONTOS TURÍSTICOS: \n");
  scanf("Pontos turísticos = %d \n" , &pontos_t2);
  printf("A quatidade de pontos turísticos é: %d \n" , pontos_t2);
@@ -68,5 +70,7 @@ int main(){
  //Cartas finalizadas
 
  return 0;
+
+}
 
 }
